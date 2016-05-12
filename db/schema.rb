3240be49ce448267c2e3f12c086f875c035cd319
Fabian Pombo <contact@fabianpombo.com>
2016-05-12 19:32:52 -0400

    removed column name from post model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511225142) do
+ActiveRecord::Schema.define(version: 20160512233016) do
 
   create_table "posts", force: :cascade do |t|
-    t.string   "name"
     t.string   "gamertag"
     t.string   "game"
     t.text     "description"
