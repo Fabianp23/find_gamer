1027937bec8146f209153d387cb9d048cf9ed9f3
Fabian Pombo <contact@fabianpombo.com>
2016-05-16 19:07:30 -0400

    added feature where depending on console (xbox one/360, ps3/ps4) different images are added to the post. Also added same feature for microphone (true or false)

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+
+  def headset_img(post)
+    if post.microphone
+      image_tag "headset.png"
+    else
+      image_tag "no_headset.png"
+    end
+  end
+
+  def console_img(post)
+    if post.console == "Xbox One" || post.console == "Xbox 360"
+      image_tag "xbox_msg.png"
+    else
+      image_tag "psn_profile.png"
+     end
+  end
+
 end
