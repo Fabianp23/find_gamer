3c7e3318a00bb10aa11634df1a37c155f69b42a6
Fabian Pombo <contact@fabianpombo.com>
2016-05-17 19:46:26 -0400

    added ajax validations for blank fields and for xbox one/360 invalid gamertag

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -20,9 +20,45 @@ function ready() {
   });
 
   $('.new_post').on('ajax:success', function(evt, postPartial){
+    clearErrors();
     $('.new_post input[type=text], .new_post textarea, .new_post select').val("");
     $(".all-posts").prepend(postPartial);
   });
+
+  $('.new-form').on('ajax:error', function(evt, xhr, status, error){
+    clearErrors();
+     errors = xhr.responseJSON;
+     var errorsArr = [];
+     if (errors.console)
+     {
+       errorsArr.push("Console " + errors.console[0]);
+     };
+     if (errors.gamertag);
+     {
+       errorsArr.push("Gamertag " + errors.gamertag[0]);
+     };
+     if (errors.game)
+     {
+       errorsArr.push("Game " + errors.game[0]);
+     };
+     if (errors.description)
+     {
+       errorsArr.push("Description " + errors.description[0]);
+     };
+     if (errors.language)
+     {
+       errorsArr.push("Language " + errors.language[0]);
+     };
+     errorsArr.forEach(addError);
+  });
+
+  function clearErrors(){
+    $('.errors').remove();
+  };
+
+  function addError(element, index, array){
+    $('.new-form').prepend("<li class='errors'>" + element + '</li>');
+  };
 
   function getAllPosts() {
     return $.ajax({
