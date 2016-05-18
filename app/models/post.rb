8e2b9aa04fd443b32165ed9d5833e2ad48ad0fd5
Michael Montero <miguelmmm809@gmail.com>
2016-05-18 17:46:58 -0400

    added playstation validation to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,9 +11,16 @@ class Post < ActiveRecord::Base
   def gamertag_truth
     if self.console == "Xbox One" || self.console == "Xbox 360"
       response = HTTParty.get("https://xboxapi.com/v2/xuid/#{gamertag}", headers: {"X-AUTH" => ENV["XBOX_TOKEN"]}).parsed_response
-        if response["success"] == false
-          errors.add(:gamertag, "is not valid")
-        end
+      if response["success"] == false
+        errors.add(:gamertag, "is not valid")
+      end
+    elsif self.console == "PlayStation 4" || self.console == "PlayStation3"
+      page = HTTParty.get("https://my.playstation.com/#{gamertag}")
+      parse_page = Nokogiri::HTML(page)
+      if  parse_page.css('.user-info h2').text == ""
+        errors.add(:gamertag, "is not valid")
+      end
+
     end
   end
 end
