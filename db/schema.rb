f902a722a3bd5481cbcdf84e11eef72877222521
Brandon <bmjohnson04@gmail.com>
2016-05-18 19:34:16 -0400

    populated db with game titles

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512233016) do
+ActiveRecord::Schema.define(version: 20160518221517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "gamertag"
